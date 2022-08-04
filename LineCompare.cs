c2730cfe8e9f2cbb77ab5be0aab631ccb31f35d6
Your Name <you@example.com>
2022-08-04 16:44:30 +0530

    [Megha] Add:  Calculate Length Of Line

---
+++ LineCompare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class LineCompare
+    {
+        public int x1 = 30, x2 = 63, y1 = 36, y2 = 70;
+        public double length;
+        public void checkLength()
+        {
+            length = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("The length Is -> " + length);
+        }
+    }
+}
