66cf63306b39286c42cff493fdcecc248baef8bb
Your Name <you@example.com>
2022-08-04 17:02:41 +0530

    [Megha] Add: UC4 Added

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welome To Line Comparision Problem");
-
             LineCompare checkLine = new LineCompare();
             checkLine.checkLength();
             checkLine.checkEquality();
